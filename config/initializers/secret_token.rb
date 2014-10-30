1c2f3c3444e435c056d70967556e887a186c0b76
jcaputo79 <jcaputo79@gmail.com>
2014-10-30 05:49:33 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PwcFlickr::Application.config.secret_key_base = 'e7b875d3f311c0d6eb0ba00c38d94de719e4757fe6d9f7e5ba47a7b7f28c9dca40565aff75d99ce495044419f81ed4ce431c75111db9f830fa51dafb20480089'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PwcFlickr::Application.config.session_store :cookie_store, key: '_pwc_flickr_session'
